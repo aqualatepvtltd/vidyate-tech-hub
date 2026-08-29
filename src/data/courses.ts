b4ba6bd5c500b1b5df17e884e091fcf9eb7ddc91
Harshit Kowsik <harshitkowsik@gmail.com>
2026-08-29 18:42:47 +0530

    update

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,13 +1,13 @@
 import {CertificationCourse } from '../types';
 
 export const CERTIFICATION_COURSES: CertificationCourse[] = [
-  {
-    id: 'full-stack-web-bootcamp',
-    name: 'Advanced Full-Stack Software Engineering & System Architecture',
-    description: 'Learn scalable backend structures, RESTful/GraphQL APIs, microservices, containerization with Docker, and automated CI/CD deployments.',
-    difficulty: 'Advance',
-    materialLink: '#',
-    testLink: '#',
-    isPaid: true
-  },
+  // {
+  //   id: 'full-stack-web-bootcamp',
+  //   name: 'Advanced Full-Stack Software Engineering & System Architecture',
+  //   description: 'Learn scalable backend structures, RESTful/GraphQL APIs, microservices, containerization with Docker, and automated CI/CD deployments.',
+  //   difficulty: 'Advance',
+  //   materialLink: '#',
+  //   testLink: '#',
+  //   isPaid: true
+  // },
 ];
